8427c6412be51b4e3d12b638426ae08e911c6f29
hendrik.loewe <hendrikloewe3@gmail.com>
2023-12-01 21:09:29 +0100

    Implement Day 1 Star 2

--- 1_trebuchet/src/main.rs
+++ 1_trebuchet/src/main.rs
@@ -1,9 +1,22 @@
 use std::{
+    collections::HashMap,
     fs::File,
     io::{BufRead, BufReader, Error},
 };
 
 fn main() -> Result<(), Error> {
+    let numbers = HashMap::from([
+        ("one", 1),
+        ("two", 2),
+        ("three", 3),
+        ("four", 4),
+        ("five", 5),
+        ("six", 6),
+        ("seven", 7),
+        ("eight", 8),
+        ("nine", 9),
+    ]);
+
     let input = File::open("input.txt")?;
     let reader = BufReader::new(input);
     let mut lines = reader.lines();
@@ -13,12 +26,18 @@ fn main() -> Result<(), Error> {
     while let Some(Ok(line)) = lines.next() {
         let mut digits: Vec<u32> = vec![];
 
-        for c in line.chars() {
+        for (i, c) in line.chars().enumerate() {
             match c {
                 '0'..='9' => {
                     digits.push(c.to_digit(10).expect("digit"));
                 }
-                _ => (),
+                _ => {
+                    if let Some((_, x)) =
+                        numbers.iter().find(|(str, _)| line[i..].starts_with(*str))
+                    {
+                        digits.push(*x);
+                    }
+                }
             }
         }
 

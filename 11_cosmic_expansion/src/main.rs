353de215f8513f734f66f16a3c0508c45e0fa331
hendrik.loewe <hendrikloewe3@gmail.com>
2023-12-11 20:46:43 +0100

    Implement Day 11 Part 2

--- 11_cosmic_expansion/src/main.rs
+++ 11_cosmic_expansion/src/main.rs
@@ -3,48 +3,54 @@ use std::{
     io::{BufRead, BufReader, Error},
 };
 
+#[derive(PartialEq, Clone)]
+enum Item {
+    Empty(usize),
+    Galaxy,
+}
+
 fn main() -> Result<(), Error> {
     let input = File::open("input.txt")?;
     let reader = BufReader::new(input);
 
-    let read_space: Vec<Vec<char>> = reader
+    let space: Vec<Vec<Item>> = reader
         .lines()
-        .map(|line| line.unwrap().chars().collect())
+        .map(|line| {
+            line.unwrap()
+                .chars()
+                .map(|c| match c {
+                    '#' => Item::Galaxy,
+                    '.' => Item::Empty(1),
+                    _ => panic!("unexpected char {c}"),
+                })
+                .collect()
+        })
         .collect();
 
-    let mut expanded_space = read_space.clone();
+    let mut expanded_space = space.clone();
 
-    let mut added_row_count = 0;
-    for (row_idx, row) in read_space.iter().enumerate() {
-        if row.contains(&'#') {
+    for (row_idx, row) in space.iter().enumerate() {
+        if row.contains(&Item::Galaxy) {
             continue;
         }
 
-        expanded_space.insert(row_idx + added_row_count, row.clone());
-
-        added_row_count += 1;
+        expanded_space[row_idx] = vec![Item::Empty(1000000); row.len()];
     }
 
-    let mut added_cols_count = 0;
     for col_idx in 0..expanded_space[0].len() {
-        if expanded_space
-            .iter()
-            .any(|row| row[col_idx + added_cols_count] == '#')
-        {
+        if space.iter().any(|row| row[col_idx] == Item::Galaxy) {
             continue;
         }
 
         for row in expanded_space.iter_mut() {
-            row.insert(col_idx + added_cols_count, row[col_idx + added_cols_count]);
+            row[col_idx] = Item::Empty(1000000);
         }
-
-        added_cols_count += 1;
     }
 
     let mut galaxy_positions: Vec<(usize, usize)> = vec![];
     for (row_idx, row) in expanded_space.iter().enumerate() {
-        for (col_idx, c) in row.iter().enumerate() {
-            if c == &'#' {
+        for (col_idx, i) in row.iter().enumerate() {
+            if i == &Item::Galaxy {
                 galaxy_positions.push((row_idx, col_idx));
             }
         }
@@ -57,13 +63,28 @@ fn main() -> Result<(), Error> {
         }
     }
 
-    dbg!(galaxy_combinations.len());
-    dbg!(galaxy_positions.len());
-
     let sum: usize = galaxy_combinations
         .iter()
         .map(|(i, j)| {
-            (((i.0 as i64) - (j.0 as i64)).abs() + ((i.1 as i64) - (j.1 as i64)).abs()) as usize
+            let mut distance = 0;
+
+            let y_range = if i.0 > j.0 { j.0..i.0 } else { i.0..j.0 };
+            for y in y_range {
+                distance += match expanded_space[y][i.1] {
+                    Item::Galaxy => 1,
+                    Item::Empty(x) => x,
+                };
+            }
+
+            let x_range = if i.1 > j.1 { j.1..i.1 } else { i.1..j.1 };
+            for x in x_range {
+                distance += match expanded_space[i.0][x] {
+                    Item::Galaxy => 1,
+                    Item::Empty(x) => x,
+                };
+            }
+
+            distance
         })
         .sum();
 
